571fa455ac9b3608f1ebe4ec1867a4b60304a966
nihadaslanli <nihadsasl@code.edu.az>
2025-01-15 18:56:08 +0400

    Add project files.

---
+++ Array 1-3/Program.cs
@@ -0,0 +1,12 @@
+﻿#region task1
+//int[] array = { -1, -2, -3, -4 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] < 0)
+//    {
+//        array[i] = Math.Abs(array[i]);
+//    }
+//}
+//Console.WriteLine(string.Join(", ", array));
+#endregion
+
